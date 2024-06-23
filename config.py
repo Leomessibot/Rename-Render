341c0a2c9679e7e71596d33a4fe0a60efe269134
Leomessibot <151534752+Leomessibot@users.noreply.github.com>
2024-06-24 00:17:43 +0400

    Update config.py

--- config.py
+++ config.py
@@ -7,30 +7,30 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
+API_ID = os.environ.get("API_ID", "26305156")
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "9937930c368c669ca905e9a95aa712f0")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7317164501:AAE-TiyWBlJaiD-QrVRZhhrXuPQrKF3Ya_c") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "VJ_Botz") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "moviebot_channel") 
 
-             # Don't Remove Credit @VJ_Botz
+             # Don't Remove Credit @cinemaworld_update 
              # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-             # Ask Doubt on telegram @KingVJ01
+             # Ask Doubt on telegram @Leomessi_10_19 
 
-DB_NAME = os.environ.get("DB_NAME", "renamevjbot")     
+DB_NAME = os.environ.get("DB_NAME", "puyoo")     
 
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://joyyus:joyyus@puyoo.vc0ax0l.mongodb.net/?retryWrites=true&w=majority&appName=puyoo")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
-START_PIC = os.environ.get("START_PIC", "https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg")
+START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/3eb1358499627bb672e84.jpg")
 
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5606411877').split()]
 
 PORT = os.environ.get("PORT", "8080")
 
-# Don't Remove Credit @VJ_Botz
+# Don't Remove Credit @cinemaworld_update 
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Ask Doubt on telegram @Leomessi_10_19 
